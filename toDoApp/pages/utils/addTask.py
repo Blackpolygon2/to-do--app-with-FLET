80963325ae5aa4646eb92fdd1d663dacc1b58cab
Blackpolygon2 <yobiojomi@gmail.com>
2024-02-22 19:56:01 -0500

    revamed the way adding task is handiled

--- toDoApp/pages/utils/addTask.py
+++ toDoApp/pages/utils/addTask.py
@@ -5,19 +5,66 @@ from .database import Task, engine, session
 # set these to be text and asing a text to the button uintead of using the button built in text
 new_object = Task
 pickDueDate = Text("pick Due Date")
-pickDueTime = Text("pick Due Time")
-pickStarttime = Text("pick Due Time")
 userPage = ft.Page
 TextDueDate = Text("Due Date", visible=False)
-TextStarttime = Text("Start Time", visible=False)
-TextDueTime = Text("Due Time", visible=False)
-pichtimebutton = ButtonStyle(
-    shape=RoundedRectangleBorder(radius=5),
-)
+reocouringtime = Text("reocuring time")
 prioritybuttonsstyle = ButtonStyle(
     shape=RoundedRectangleBorder(radius=5),
 )
 bgcolorforfilledbuttons = "#6a7a67"
+
+reocuringColumn = Column(
+    [
+        Text("days of the week to reapet"),
+        Row(
+            [
+                SegmentedButton(
+
+                    show_selected_icon=False,
+                    # style=prioritybuttonsstyle,
+                    on_change=lambda e: choose_day_of_week_reocuring(e),
+                    allow_multiple_selection=True,
+                    allow_empty_selection=True,
+                    segments=[
+                        ft.Segment(
+                            value="1",
+                            label=ft.Text("mon"),
+                        ),
+                        ft.Segment(
+                            value="2",
+                            label=ft.Text("tue"),
+                        ),
+                        ft.Segment(
+                            value="3",
+                            label=ft.Text("wed"),
+                        ),
+                        ft.Segment(
+                            value="4",
+                            label=ft.Text("thur"),
+                        ),
+                        ft.Segment(
+                            value="5",
+                            label=ft.Text("fri"),
+                        ),
+                        ft.Segment(
+                            value="6",
+                            label=ft.Text("sat"),
+                        ),
+                        ft.Segment(
+                            value="7",
+                            label=ft.Text("sun"),
+                        ),
+
+
+                    ],
+                ),
+            ], scroll=ScrollMode.AUTO
+        ), Text("start time"),
+        Row([
+            OutlinedButton("pick start time", on_click=lambda _: Text(
+                "pick start time")), reocouringtime
+        ])
+    ])
 
 
 def change_date(e):
@@ -29,30 +76,9 @@ def change_date(e):
     TextDueDate.update()
 
 
-def time_changed_due(e):
-    new_object.tasktimeend = e.control.value
-    global pickDueTime
-    pickDueTime.value = str(e.control.value)
-    pickDueTime.update()
-    TextDueTime.visible = True
-    TextDueTime.update()
-
-
-def time_changed_start(e):
-    new_object.tasktimestart = e.control.value
-    global pickStarttime
-    pickStarttime.value = str(e.control.value)
-    pickStarttime.update()
-    TextStarttime.visible = True
-    TextStarttime.update()
-
-
 def dismissed(e):
-    pass
-
-
-def time_changed(timePicker, e):
-    pass
+    global pickDueDate
+    pickDueDate = Text("pick Due Date")
 
 
 def choose_priority(priority,):
@@ -62,8 +88,6 @@ def choose_priority(priority,):
 
 def appendpopups(page):
     page.overlay.append(date_picker)
-    page.overlay.append(time_picker_start)
-    page.overlay.append(time_picker_due)
 
 
 def add_task_name(e):
@@ -75,26 +99,6 @@ def add_task_description(e):
     taskdescription = e.control.value
     new_object.taskdescription = taskdescription
 
-
-def addTaskFunction():
-    new_object = Task(taskname="wash hands", taskdescription="wash your hands", tasktimestart="10:00",
-                      tasktimeend="11:00", taskpriority=1, istaskcompleted=False, taskdaydue="2023-05-01", istaskreocuring=False)
-    session.add(new_object)
-    session.commit()
-
-    # Example: Querying data from the database
-    result = session.query(Task).filter_by(taskname='wash hands')
-    print(result)
-
-
-time_picker_start = TimePicker(
-    on_change=time_changed_start,
-    on_dismiss=dismissed,
-)
-time_picker_due = TimePicker(
-    on_change=time_changed_due,
-    on_dismiss=dismissed,
-)
 
 date_picker = DatePicker(
     on_change=change_date,
@@ -110,32 +114,17 @@ def taskAddPopupColumn(page):
                       on_change=lambda _: add_task_name,),
             TextField(label="Task Description (Optional)", multiline=True,
                       on_change=lambda _: add_task_description,),
+            Text("Time Managment (Optional)"),
             Row(
                 [
                     Column(
                         [
                             TextDueDate,
                             OutlinedButton(content=pickDueDate, on_click=lambda _: date_picker.pick_date(
-                            ), style=pichtimebutton),
+                            ),),
                         ]
                     ),
-                    Column(
-                        [
-                            TextStarttime,
-                            OutlinedButton(content=pickStarttime, on_click=lambda _: time_picker_start.pick_time(
-                            ), style=pichtimebutton)
-                        ]
-                    ),
-                    Column(
-                        [
-                            TextDueTime,
-                            OutlinedButton(content=pickDueTime, on_click=lambda _: time_picker_due.pick_time(
-                            ), style=pichtimebutton),
-                        ]
-                    ),
-
-
-                ], alignment=MainAxisAlignment.CENTER, spacing=10
+                ], alignment=MainAxisAlignment.CENTER, spacing=10, scroll=ScrollMode.AUTO
             ),
             Column(
                 [
@@ -165,11 +154,14 @@ def taskAddPopupColumn(page):
                 ]
             ),
 
-
+            Switch(label="Task Reocuring", value=False),
             ElevatedButton(icon=icons.CHECK,
                            on_click=addTaskFunction, text="Add Task",),
+            reocuringColumn,
 
-        ], spacing=30, alignment=MainAxisAlignment.CENTER, horizontal_alignment=MainAxisAlignment.CENTER
+
+
+        ], spacing=30, alignment=MainAxisAlignment.CENTER, horizontal_alignment=MainAxisAlignment.CENTER, height=500, scroll=ScrollMode.AUTO
     )
 
 
@@ -183,10 +175,24 @@ def PopupAddTaskBottomSheet(page):
         ft.Container(
             content=taskAddPopupColumn(page),
             padding=15,
+            height=500,
         ),
         open=True,
         on_dismiss=bs_dismissed,
+        enable_drag=True,
+        show_drag_handle=True,
     )
+
+
+def addTaskFunction():
+    new_object = Task(taskname="wash hands", taskdescription="wash your hands", tasktimestart="10:00",
+                      tasktimeend="11:00", taskpriority=1, istaskcompleted=False, taskdaydue="2023-05-01", istaskreocuring=False)
+    session.add(new_object)
+    session.commit()
+
+    # Example: Querying data from the database
+    result = session.query(Task).filter_by(taskname='wash hands')
+    print(result)
 
 
 def addTaskFunction():
